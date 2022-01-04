98e086d3ac04e40b1c208b3bdbb9f2e77c334472
fypan2018 <freddy4life@163.com>
2022-01-04 18:31:34 +0800

    Update exam.js

--- exam.js
+++ exam.js
@@ -1,6 +1,5 @@
 // Set the date we're counting down to
-//var countDownDate = new Date("Jan 11, 2022 15:30:00").getTime();
-var countDownDate = new Date("Jan 4, 2022 18:28:50").getTime();
+var countDownDate = new Date("Jan 11, 2022 15:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(countdown, 1000);
