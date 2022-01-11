54d58756a1aa1f7918a3ecadb6a84009fddce048
fypan2018 <freddy4life@163.com>
2022-01-11 10:39:56 +0800

    updated

--- exam.js
+++ exam.js
@@ -1,10 +1,16 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 11, 2022 15:30:00").getTime();
 
+
 // Update the count down every 1 second
 var x = setInterval(countdown, 1000);
+var isTurnOn=true;
 
 //==========================================================
+function shift(){
+  isTurnOn=!isTurnOn;
+} 
+
 function countdown() {
   var now = new Date().getTime(); //unit: millisecond
   var distance = countDownDate - now;
@@ -15,9 +21,14 @@ function countdown() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML =  hours + "时 " + minutes + "分 " + seconds + "秒 ";
+  if(isTurnOn==true)
+    document.getElementById("demo").innerHTML =  "倒计时： "+hours + "时 " + minutes + "分 " + seconds + "秒 ";
+  else
+    document.getElementById("demo").innerHTML =  "";
+  
     var x = document.body;
 	if(distance <1000*10){
+    isTurnOn=1
 	document.getElementById("demo").innerHTML = seconds + "秒 " + "请及时提交！！";	
 	document.getElementById("demo").style.fontSize=20+'px';
     x.style.backgroundColor = x.style.backgroundColor == "yellow" ? "red" : "yellow";
@@ -31,3 +42,4 @@ function countdown() {
 	x.style.backgroundColor = "black";
   }
 }
+
